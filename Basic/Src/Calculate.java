90631b07f52f3aa9609f678a949e92a123784338
Baibhav <paudelbaibhav0@gmail.com>
2022-06-14 23:58:25 +0545

    calculating function

---
+++ Basic/Src/Calculate.java
@@ -0,0 +1,15 @@
+package Basic.Src;
+
+public class Calculate {
+    public void calculate(int a, int b){
+        System.out.println("add is " + (a + b));
+        System.out.println("multiplication is " + a * b);
+        System.out.println("substract is " + (a - b));
+        System.out.println("division is " + a / b);
+    }
+
+    public static void main(String[] args){
+        Calculate calc = new Calculate();
+        calc.calculate(20, 2);
+    }
+}
